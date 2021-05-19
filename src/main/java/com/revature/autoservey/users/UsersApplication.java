da3b29f393a6faf4aab9de645843ec21084eba37
Thomas An <tommyan97@msn.com>
2021-05-19 08:24:00 -0700

    moving some more stuff

--- src/main/java/com/revature/autoservey/users/UsersApplication.java
+++ src/main/java/com/revature/autoservey/users/UsersApplication.java
@@ -1,4 +1,4 @@
-package com.revature.users.autosurvey;
+package com.revature.autoservey.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
