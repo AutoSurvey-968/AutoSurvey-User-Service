500a0dadaef50c5f9c8547f0330730da92edf29d
Thomas An <tommyan97@msn.com>
2021-05-19 11:08:56 -0700

    commiting before merge

--- src/main/java/com/revature/autosurvey/users/controllers/UserController.java
+++ src/main/java/com/revature/autosurvey/users/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.revature.autosurvey.users.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,14 +12,17 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.revature.autosurvey.users.beans.User;
 import com.revature.autosurvey.users.services.UserService;
 
 import reactor.core.publisher.Mono;
 
-@RestController
-@RequestMapping(value = "/users")
+@Component
+//@RestController
+//@RequestMapping(value = "/users")
 public class UserController {
 	@Autowired
 	private UserService userService;
@@ -26,8 +31,14 @@ public class UserController {
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
+	
+	public Mono<ServerResponse> getUsers(ServerRequest req) {
+		return ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(userService.getAllUsers(), User.class);
+	}
 
-	@PostMapping
+//	@PostMapping
 	public Mono<ResponseEntity<User>> addUser(@RequestBody User user) {
 		return userService.addUser(user).defaultIfEmpty(emptyUser).map(u -> {
 			if (u.getUsername() == null) {
@@ -37,27 +48,27 @@ public class UserController {
 		});
 	}
 
-	@PutMapping
+//	@PutMapping
 	public void login() {
 
 	}
 
-	@GetMapping("/{id}")
+//	@GetMapping("/{id}")
 	public void getUserById() {
 
 	}
 
-	@GetMapping
+//	@GetMapping
 	public void getUserEmail() {
 
 	}
 
-	@PutMapping("/{id}")
+//	@PutMapping("/{id}")
 	public void updateUser() {
 
 	}
 
-	@DeleteMapping("/{id}")
+//	@DeleteMapping("/{id}")
 	public void deleteUser() {
 
 	}
---
+++ src/main/java/com/revature/autosurvey/users/routes/UserRoutes.java
@@ -0,0 +1,27 @@
+package com.revature.autosurvey.users.routes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.revature.autosurvey.users.controllers.UserController;
+
+
+
+@Configuration
+public class UserRoutes {
+	@Autowired
+	UserController uc;
+	
+	@Bean
+	@Primary
+	RouterFunction<ServerResponse> routes() {
+		return RouterFunctions.route(RequestPredicates
+				.GET("/users"), uc::getUsers);
+	}
+}
