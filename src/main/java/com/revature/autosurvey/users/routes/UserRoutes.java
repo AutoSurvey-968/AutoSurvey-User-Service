bbf6f0c5b7bd15287022b615dea96679dfaa1f28
Thomas An <tommyan97@msn.com>
2021-05-25 09:29:53 -0700

    removing unused import

--- src/main/java/com/revature/autosurvey/users/routes/UserRoutes.java
+++ src/main/java/com/revature/autosurvey/users/routes/UserRoutes.java
@@ -3,7 +3,6 @@ package com.revature.autosurvey.users.routes;
 import java.util.NoSuchElementException;
 
 import org.apache.http.HttpStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
