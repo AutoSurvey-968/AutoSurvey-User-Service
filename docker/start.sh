61e36bfab8f3ead8b86e97552de0dc2a764da7f7
DuskDaleSpider <dakota.ray.clark@gmail.com>
2021-05-27 14:25:08 -0400

    Fixed start to place credentials file in right place

--- docker/start.sh
+++ docker/start.sh
@@ -2,5 +2,5 @@
 #Decode the passed in truststore env variable
 mkdir -p src/main/resources
 echo "$TRUSTSTORE_ENCODED" | base64 -d > src/main/resources/cassandra_truststore.jks
-echo "$CREDENTIALS_JSON_ENCODED" | base64 -d > src/main/resources/$CREDENTIALS_JSON
+echo "$CREDENTIALS_JSON_ENCODED" | base64 -d > $CREDENTIALS_JSON
 java -jar user-service.jar
