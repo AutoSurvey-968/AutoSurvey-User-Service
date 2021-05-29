cc8873d39d4165268515f696963491a8f67516f9
Thomas An <tommyan97@msn.com>
2021-05-29 09:49:18 -0700

    fixing code smells

--- src/main/java/com/revature/autosurvey/users/beans/UserWithToken.java
+++ src/main/java/com/revature/autosurvey/users/beans/UserWithToken.java
@@ -8,7 +8,7 @@ public class UserWithToken extends User {
 
 	private static final long serialVersionUID = -87467209632178467L;
 	
-	private String token;;
+	private String token;
 	
 	public UserWithToken() {
 		super();
@@ -27,6 +27,31 @@ public class UserWithToken extends User {
 
 	public void setToken(String token) {
 		this.token = token;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((token == null) ? 0 : token.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserWithToken other = (UserWithToken) obj;
+		if (token == null) {
+			if (other.token != null)
+				return false;
+		} else if (!token.equals(other.token))
+			return false;
+		return true;
 	}
 	
 }
